0746843f0692b82d392d22ce216c80364bb455a1
for <nithins674@gmail.com>
2024-02-16 15:06:24 +0000

    Add isAdmin middleware for role-based authorization

---
+++ middlewares/isAdmin.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func IsAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, exists := c.Get("role")
+
+		if !exists {
+			c.JSON(500, gin.H{"error": "Role information not found", "data": "role information not found"})
+			c.Abort()
+			return
+		}
+
+		if role != "admin" {
+			c.JSON(401, gin.H{"error": "unauthorized", "data": "user is not an admin"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
