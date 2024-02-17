7a0fb8aaee0be854dc5742345a5c76d8af118a36
for <nithins674@gmail.com>
2024-02-17 17:52:49 +0000

    Add isStudent middleware to check if user is a student

---
+++ middlewares/isStudent.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// IsStudent is a middleware to check if the user is a student
+
+func IsStudent() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, exists := c.Get("role")
+
+		if !exists {
+			c.JSON(500, gin.H{"error": "Role information not found", "data": "role information not found"})
+			c.Abort()
+			return
+		}
+
+		if role != "student" {
+			c.JSON(401, gin.H{"error": "unauthorized", "data": "user is not a student"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
