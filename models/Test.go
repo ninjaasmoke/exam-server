0b69342e2e1102a996ed6fede2c14929478f2c52
for <nithins674@gmail.com>
2024-02-17 18:40:31 +0000

    Update Test model to use question IDs instead of questions

--- models/Test.go
+++ models/Test.go
@@ -12,7 +12,8 @@ type Test struct {
 	Subject      string     `json:"subject"`
 	Chapter      string     `json:"chapter"`
 	Level        string     `json:"level"`
-	Questions    []Question `json:"questions" gorm:"many2many:test_questions;not null"`
+	QuestionIDs  []uint     `json:"question_ids" gorm:"-"`
+	Questions    []Question `json:"-" gorm:"many2many:test_questions"`
 	TotalMarks   int        `json:"total_marks"`
 	TotalTime    int        `json:"total_time"`
 	Instructions string     `json:"instructions"`
