7d8811403dbaff37d9443e6bc265440b13b5b018
for <nithins674@gmail.com>
2024-02-17 13:28:25 +0000

    Add Question model to the project

---
+++ models/Question.go
@@ -0,0 +1,21 @@
+package models
+
+import "gorm.io/gorm"
+
+// A test question which is MCQ and can have anywhere from 2 to 6 options.
+
+type Question struct {
+	gorm.Model
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Question string `json:"question"`
+	Option1  string `json:"option1"`
+	Option2  string `json:"option2"`
+	Option3  string `json:"option3"`
+	Option4  string `json:"option4"`
+	Option5  string `json:"option5"`
+	Option6  string `json:"option6"`
+	Answer   string `json:"answer"`
+	Subject  string `json:"subject"`
+	Chapter  string `json:"chapter"`
+	Level    string `json:"level"`
+}
