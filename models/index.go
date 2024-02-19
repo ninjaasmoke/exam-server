1d99b3669b76723e924e1f82fe535f39f8834bfa
for <nithins674@gmail.com>
2024-02-19 22:54:33 +0000

    Add error handling and logging in InitDB function

--- models/index.go
+++ models/index.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"log"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -18,25 +19,34 @@ type Config struct {
 
 var DB *gorm.DB
 
-func InitDB(cfg Config) {
+func migrateModels(db *gorm.DB, models ...interface{}) error {
+	for _, model := range models {
+		if err := db.AutoMigrate(model); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InitDB(cfg Config) error {
 
 	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode)
 
 	// db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to connect to the database: %v", err)
+		return err
 	}
 
-	if err := db.AutoMigrate(&User{}); err != nil {
-		panic(err)
-	}
-
-	if err := db.AutoMigrate(&Test{}); err != nil {
-		panic(err)
+	if err := migrateModels(db, &User{}, &Test{}, &Question{}, &Response{}); err != nil {
+		log.Fatalf("Failed to migrate models: %v", err)
+		return err
 	}
 
 	fmt.Println("Migrated database")
 
 	DB = db
+
+	return nil
 }
