858d61d9dc14bfad42f23362b9bafdb9e8fdc600
for <nithins674@gmail.com>
2024-02-19 22:54:28 +0000

    Add UserID and UserName fields to Claims struct

--- models/Claims.go
+++ models/Claims.go
@@ -3,6 +3,8 @@ package models
 import "github.com/dgrijalva/jwt-go"
 
 type Claims struct {
-	Role uint `json:"role"`
+	Role     uint   `json:"role"`
+	UserID   uint   `json:"user_id"`
+	UserName string `json:"username" gorm:"column:username"`
 	jwt.StandardClaims
 }
