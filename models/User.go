36c74765c90b4a49ea788942eaf3958072cdf69d
for <nithins674@gmail.com>
2024-02-19 22:54:52 +0000

    Update User model to include Responses

--- models/User.go
+++ models/User.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	UserName string `json:"username" gorm:"column:username"`
-	Role     uint   `json:"role"`
-	Deleted  bool   `json:"deleted"`
+	Name      string     `json:"name"`
+	Email     string     `json:"email"`
+	Password  string     `json:"password"`
+	UserName  string     `json:"username" gorm:"column:username"`
+	Role      uint       `json:"role"`
+	Deleted   bool       `json:"deleted"`
+	Responses []Response `gorm:"foreignKey:UserID"`
 }
