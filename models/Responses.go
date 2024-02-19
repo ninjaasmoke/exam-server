4a6473f929c22ec35781f7ad581c2e10b578b6bf
for <nithins674@gmail.com>
2024-02-19 23:09:18 +0000

    Fix typo in Responses.go

--- models/Responses.go
+++ models/Responses.go
@@ -8,7 +8,7 @@ type Response struct {
 	gorm.Model
 	UserID     uint   `json:"user_id" gorm:"non null;uniqueIndex:unique_response;foreignKey:UserID"`
 	TestID     uint   `json:"test_id" gorm:"non null;uniqueIndex:unique_response;foreignKey:TestID"`
-	QuestionID uint   `json:"question_id" gorm:"non null;uniqueIndex:unique_response";foreignKey:QuestionID`
+	QuestionID uint   `json:"question_id" gorm:"non null;uniqueIndex:unique_response;foreignKey:QuestionID"`
 	Response   string `json:"response"`
 	Deleted    bool   `json:"deleted"`
 }
